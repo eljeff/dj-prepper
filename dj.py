74f8eefc93b2155bec2a2aa73710a913eaad3eb9
Jeff Cooper <jeffocooper@gmail.com>
2023-04-03 21:49:38 -0400

    cleanup files, add bool to timestretch or resample

--- dj.py
+++ dj.py
@@ -3,12 +3,14 @@ import DirScanner
 import GetBPM
 import os
 
+target_bpm = 111.0
+pattern_match = "*.mp3"
+timestretch = True
 dir_to_scan = "process"
-scanner = DirScanner.DirScanner()
-files = scanner.scan(dir_to_scan, "*.mp3")
 outdir = "processed"
 
-target_bpm = 111.0
+scanner = DirScanner.DirScanner()
+files = scanner.scan(dir_to_scan, pattern_match)
 
 for f in files:
     print("processing " + f)
@@ -18,17 +20,20 @@ for f in files:
     bpm, sr = GetBPM.getBPMLibrosa(f, 120.0, 50.0)
 
     modifier = target_bpm / bpm
-    new_bpm = bpm * modifier
+    new_sr = modifier * sr
 
-    print(str(bpm) + " -- " + str(modifier) + " -- " + str(new_bpm))
+    print(str(bpm) + " -- " + str(modifier) + " -- " + str(new_sr))
     new_path = path_with_bpm.replace(dir_to_scan, outdir)
     
-    new_sr = modifier * sr
-    print("resampling to " + str(new_sr))
     print("saving to " + new_path)
 
-    # ff = ffmpy.FFmpeg(inputs={f: None}, outputs={new_path: ["-filter:a", "asetrate=" + str(new_sr)]}) #timestretch
-    setrate_filter = "[a]asetrate=" + str(new_sr) + "[b]"
-    resample_filter = "[b]aresample=44100"
-    ff = ffmpy.FFmpeg(inputs={f: None}, outputs={new_path: ["-y", "-filter_complex:a", setrate_filter + ", " + resample_filter]})
+    if timestretch:
+        print("timestretching to " + str(new_sr))
+        ff = ffmpy.FFmpeg(inputs={f: None}, outputs={new_path: ["-filter:a", "asetrate=" + str(new_sr)]}) #timestretch
+    else:
+        print("resampling to " + str(new_sr))
+        setrate_filter = "[a]asetrate=" + str(new_sr) + "[b]"
+        resample_filter = "[b]aresample=44100"
+        ff = ffmpy.FFmpeg(inputs={f: None}, outputs={new_path: ["-y", "-filter_complex:a", setrate_filter + ", " + resample_filter]})
+
     ff.run()
