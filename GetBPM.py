6c5a28141cfdd20eea6ff05fd38502038876cac6
Jeff Cooper <jeffocooper@gmail.com>
2023-07-19 14:24:46 -0400

    use duration to decide if we should only analyze one minute of the file

--- GetBPM.py
+++ GetBPM.py
@@ -1,13 +1,21 @@
 import librosa
 
 def getBPMLibrosa(f, target):
-    y, sr = librosa.load(f, sr=None, mono=True, offset=60, duration=60)
-    beatinfo = librosa.beat.tempo(y=y, sr=sr, start_bpm=int(target), hop_length=64)#, ac_size=10, hop_length=128)
+    file_duration = librosa.get_duration(filename=f)
+    offset = 60
+    duration = 60
+    if file_duration < (offset + duration):
+        offset = 0
+        duration = file_duration
+
+    y, sr = librosa.load(f, sr=None, mono=True, offset=offset, duration=duration)
+    beatinfo = librosa.beat.tempo(y=y, sr=sr, start_bpm=int(target), hop_length=70)
     bpm = beatinfo[0]
-    print("guessed " + str(bpm))
+    print("guessed bpm " + str(bpm))
     
     return keepBPMInRangeScale(bpm, target), sr
 
+#offset and duration in seconds
 def getBPMLibrosaTweak(f, offset, duration, target, range):
     y, sr = librosa.load(f, sr=None, offset=offset, duration=duration)
     beatinfo = librosa.beat.tempo(y=y, sr=sr)
